e8f5223759b6811b8c2e91cf5192dec75b9ef257
JVFrancaLisboa <logiccoder88@gmail.com>
2025-03-27 13:07:57 -0300

    36. 0504 Definindo os Métodos de Validação e Executando a Aplicação

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/model/controllers/MathController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/model/controllers/MathController.java
@@ -19,12 +19,17 @@ public class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
-    private Double convertToDouble(String numberOne) {
-        return 1D;
+    private Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) throw new IllegalArgumentException();
+        String number = strNumber.replace(",", ".");
+        return Double.parseDouble(number);
     }
 
-    private boolean isNumeric(String number) {
-        return true;
+    private boolean isNumeric(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) return false;
+        String number = strNumber.replace(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
     }
 
     // http://localhost:8080/math/subtraction/3/5
