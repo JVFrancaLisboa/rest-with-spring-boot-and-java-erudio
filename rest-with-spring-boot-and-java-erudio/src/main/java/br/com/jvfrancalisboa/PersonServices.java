08f19acc51150526fe9941f88e8806b0e3213e74
JVFrancaLisboa <logiccoder88@gmail.com>
2025-05-06 20:33:44 -0300

    47 - Retornando Listas de Objetos em JSON

--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/PersonServices.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/PersonServices.java
@@ -3,6 +3,8 @@ package br.com.jvfrancalisboa;
 import br.com.jvfrancalisboa.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,16 @@ public class PersonServices {
 
     private AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll(){
+        var persons = new ArrayList<Person>();
+        for (int i = 1; i <= 8; i++) {
+            Person person = MockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
 
     public Person findById(String id){
         logger.info("Finding one person");
@@ -21,6 +33,18 @@ public class PersonServices {
         person.setLastName("Costa");
         person.setAddress("Uberlândia");
         person.setGender("Male");
+        return person;
+    }
+
+    private Person MockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("FirstName "+i);
+        person.setLastName("LastName "+i);
+        person.setAddress("Some Address in Brazil");
+        boolean g = false;
+        if (i % 2 == 0) g = true;
+        person.setGender(g ? "Female" : "Male");
         return person;
     }
 }
--- rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/br/com/jvfrancalisboa/controllers/PersonController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
@@ -16,11 +18,21 @@ public class PersonController {
     @Autowired
     private PersonServices personServices;
 
-    @RequestMapping(value = "/{id}",
+    @RequestMapping(
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll(){
+        return personServices.findAll();
+    }
+
+    @RequestMapping(
+            value = "/{id}",
             method = RequestMethod.GET,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     public Person findById(@PathVariable("id") String id){
         return personServices.findById(id);
     }
+
 }
